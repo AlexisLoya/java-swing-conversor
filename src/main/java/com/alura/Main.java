d09c36e85f217dac2f7cb7acec76936106dbf71f
AlexisLoya <alexloy117@gmail.com>
2023-04-09 23:13:16 -0600

    change display inputs

--- src/main/java/com/alura/Main.java
+++ src/main/java/com/alura/Main.java
@@ -122,8 +122,8 @@ public class Main extends JFrame {
         panel.add(tempInput1TextField);
         panel.add(tempUnit1ComboBox);
         panel.add(new JLabel("to"));
-        panel.add(tempInput2TextField);
         panel.add(tempUnit2ComboBox);
+        panel.add(tempInput2TextField);
         panel.add(tempConvertButton);
         panel.add(tempResultTextArea);
 
@@ -167,9 +167,9 @@ public class Main extends JFrame {
 
         panel.add(currencyInput1TextField);
         panel.add(currencyUnit1ComboBox);
-        panel.add(currencyInput2TextField);
         panel.add(new JLabel("to"));
         panel.add(currencyUnit2ComboBox);
+        panel.add(currencyInput2TextField);
         panel.add(currencyConvertButton);
         panel.add(currencyResultTextArea);
         panel.add(updateRatesButton);
